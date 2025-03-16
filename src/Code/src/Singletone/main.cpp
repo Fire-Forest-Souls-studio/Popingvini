c1e0af54f8dcc199643dbd2cb922dc44a6029d1c
Fire Forest Souls <stepan.sniff@gmail.com>
2025-03-16 16:24:30 +0300

    Update LICENSE

--- src/Code/src/Singletone/main.cpp
+++ src/Code/src/Singletone/main.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2024 FireForestSouls
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 #include "../../includes/Singletone/names.h"
 #include "../../includes/Singletone/pc_mouse.h"
 #include "../../includes/Singletone/window_and_styles.h"
